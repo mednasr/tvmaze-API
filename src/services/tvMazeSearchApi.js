d928497f464b0e9e6963456fdb81148382e66576
Mohamed hedi Nasr <mednasr@users.noreply.github.com>
2022-03-28 11:27:02 +0200

    Update api calls

--- src/services/tvMazeSearchApi.js
+++ src/services/tvMazeSearchApi.js
@@ -1,5 +1,5 @@
 import { handleResponse, handleError } from './apiUtils';
-const baseUrl = 'http://api.tvmaze.com/search/shows?q=';
+const baseUrl = 'https://api.tvmaze.com/search/shows?q=';
 export function getTVShowbyNames(query) {
   return fetch(baseUrl + query).then(handleResponse).catch(handleError);
 }
